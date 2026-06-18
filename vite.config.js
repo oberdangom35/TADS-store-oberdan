f6323d8650b0ecad71adb4611b2a901582d6fadb
oberdangom35 <Oberdan.gomes@outlook.com>
2026-06-17 21:32:26 -0300

    Limpeza e otimizações no código e confifurações do Vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    // Remove apenas 'log' e 'debugger' no build de produção.
-    // Mantém 'error' e 'warn' ativos para monitoramento seguro.
-    drop: ['console', 'debugger']
-  }
+  plugins: [react()]
 })
